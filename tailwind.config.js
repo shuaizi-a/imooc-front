054a1bd7e2c8204b5b4ecc4d9bb15db6fef94c8e
张帅 <mi_shuai.zhang@mihoyo.com>
2022-12-21 19:15:15 +0800

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
     extend: {
-      // height: {
-      //   header: '72px',
-      //   main: 'calc(100vh - 72px)'
-      // },
+      height: {
+        header: '72px',
+        main: 'calc(100vh - 72px)'
+      },
       fontSize: {
         xs: ['0.25rem', '0.35rem'],
         sm: ['0.35rem', '0.45rem'],
@@ -20,25 +20,25 @@ module.exports = {
         'l-white': '-10px 0 10px white',
         'l-zinc': '-10px 0 10px #18181b'
       },
-      // colors: {
-      //   main: '#f44c58',
-      //   'hover-main': '#f32836',
-      //   'success-100': '#F2F9EC',
-      //   'success-200': '#E4F2DB',
-      //   'success-300': '#7EC050',
-      //   'warn-100': '#FCF6ED',
-      //   'warn-200': '#F8ECDA',
-      //   'warn-300': '#DCA550',
-      //   'error-100': '#ED7456',
-      //   'error-200': '#f3471c',
-      //   'error-300': '#ffffff'
-      // },
-      // backdropBlur: {
-      //   '4xl': '240px'
-      // },
-      // variants: {
-      //   scrollbar: ['dark']
-      // }
+      colors: {
+        main: '#f44c58',
+        'hover-main': '#f32836',
+        'success-100': '#F2F9EC',
+        'success-200': '#E4F2DB',
+        'success-300': '#7EC050',
+        'warn-100': '#FCF6ED',
+        'warn-200': '#F8ECDA',
+        'warn-300': '#DCA550',
+        'error-100': '#ED7456',
+        'error-200': '#f3471c',
+        'error-300': '#ffffff'
+      },
+      backdropBlur: {
+        '4xl': '240px'
+      },
+      variants: {
+        scrollbar: ['dark']
+      }
     }
   },
   plugins: [
